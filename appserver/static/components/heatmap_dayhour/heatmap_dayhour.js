d40bbceb348b37f4e39637e1a98beb56c55af0e5
bernie macias <berniem@zillow.com>
2016-08-21 22:27:01 -0700

    adding custom viz template info

---
+++ appserver/static/components/heatmap_dayhour/heatmap_dayhour.js
@@ -0,0 +1,63 @@
+/**
+ * Created by berniem on 8/21/16.
+ */
+define(function(require, exports, module) {
+
+  // loading all require modules
+  var _ = require("underscore");
+  var d3 = require("../d3/d3");
+  var colorbrewer = require("../d3/colorbrewer");
+  var mvc = require("splunkjs/mvc");
+  var css = require("css!./heatmap_dayhour.css");
+  // Base class for custom views
+  var SimpleSplunkView = require("splunkjs/mvc/simplesplunkview");
+
+  // Define the custom view class
+  var Heatmaphourday = SimpleSplunkView.extend({
+    className: "splunk-headmap-day-hour-chart",
+    options: {
+      "managerid": null,
+      "data": "preview",
+      "gridSize": 24,
+      "height": 450,
+      "hourField": "time",
+      "dayField": "day",
+      "valueField": "value",
+      "colorbrewPalette": "Paired",
+      "paletteNumber": 12
+    },
+
+    output_mode: "json",
+
+    initialize: function() {
+      SimpleSplunkView.prototype.initialize.apply(this, arguments);
+
+      // Set up resize callback.
+      $(window).resize(_.debounce(_.bind(this._handleResize, this), 20));
+    },
+
+    _handleResize: function() {
+      this.render();
+    },
+
+    createView: function() {
+      return true;
+    },
+
+    // Making the data look how we want it to for updateView to do its job
+    formatData: function(data) {
+      var formattedData = [];
+
+
+      return formattedData;
+    },
+
+    // update view as search finished
+    updateView: function(viz, data) {
+      this.$el.html("");
+      var id = this.id;
+
+    }
+  });
+  return Heatmaphourday;
+});
