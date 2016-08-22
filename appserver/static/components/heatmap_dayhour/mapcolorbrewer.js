9c57cbbb78d84af9240ecd2d471d44ad25214eeb
bernie macias <berniem@zillow.com>
2016-08-22 12:28:45 -0700

    add mapcolorbrew basics with simple adding of text

--- appserver/static/components/heatmap_dayhour/mapcolorbrewer.js
+++ appserver/static/components/heatmap_dayhour/mapcolorbrewer.js
@@ -3,22 +3,21 @@
  */
 /**
  * Created by berniem on 4/6/16.
+ * Color mapcolorbrewer borrowed from http://colorbrewer2.org/; colors by Cynthia Brewer
  */
 
 define(function(require, exports, module) {
   var _ = require("underscore");
+  var $ = require("jquery");
   var SimpleSplunkView = require("splunkjs/mvc/simplesplunkview");
   var mvc = require("splunkjs/mvc");
-  //
-  // All Additional JS modules
-  //
+  var d3 = require("../d3/d3");
+  var colorbrewer = require("../d3/colorbrewer");
+  var css = require("css!./mapcolorbrewer.css");
 
   // Define the customclass view
   var Mapcolorbrewer = SimpleSplunkView.extend({
     className: "splunk-map-color-brew",
-    //
-    // Custom VIZ options used in XML div data-options
-    //
     options: {
       "managerid": null,
       "data": "preview"
@@ -46,15 +45,17 @@ define(function(require, exports, module) {
       // structure for Data VIZ
       //
       var formattedData = [];
-
-
+      formattedData.push(data); // Data not used but used to ticket viz to rendering
       return formattedData;
     },
     updateView: function(viz, data) {
       //
       // ADD VIZ HERE
       //
-
+      this.$el.html(""); // Clears waiting for Data
+      var id = this.id;
+      console.log(id)
+      $("#"+id).append("<h2>This is my Custom Viz</h2>")
     }
   });
   return Mapcolorbrewer;
